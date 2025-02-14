6e6d56c5c104290e55ea4d8580e5cc5785696c56
root <root@DESKTOP-STUO4KN>
2025-02-14 10:26:07 -0600

    create two files: comments.py and varaibles.py

---
+++ comments.py
@@ -0,0 +1,21 @@
+# We are going create different variables and print the variables to the console(terminal)  =====> single coments
+# We are going create different variables and print the variables to the console(terminal)  =====> single coments
+# We are going create different variables and print the variables to the console(terminal)  =====> single coments
+# We are going create different variables and print the variables to the console(terminal)  =====> single coments
+# We are going create different variables and print the variables to the console(terminal)  =====> single coments
+
+'''
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+'''
+
+"""
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+We are going create different variables and print the variables to the console(terminal)  =====> multiline coments
+"""
