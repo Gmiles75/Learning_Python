e07c46c8e64b8634158d819899f8556396ddb1e8
root <root@DESKTOP-STUO4KN>
2025-02-19 11:04:47 -0600

    using pseudo code to learn variables

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+# write a print statement  'Welcome to my profile'
+# create a variable called 'firstName' and assigned your first name as a value to it
+# create another variable called 'lastName' and assign your last name as a value to it
+# create another variable called 'favouriteColour' and assign your favourite colour as a value to it
+# print firstName
+# print lastName
+# print favouriteColour
+
+print("Welcome to my profile")
+firstName = "Gregory"
+lastName = "Miles"
+favouriteColour = "hunter green"
+print('First Name:',firstName)
+print('Last Name:',lastName)
+print('Favourite Colour:',favouriteColour)
