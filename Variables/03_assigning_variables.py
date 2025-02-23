fcfba7b7112d456071bed550a865ecad351dbaa0
root <root@DESKTOP-STUO4KN>
2025-02-23 07:34:35 -0600

    Learn how to assign multiple vaiables and move files to new directory called variables

---
+++ Variables/03_assigning_variables.py
@@ -0,0 +1,20 @@
+# Assigning multiple variables
+# One value to multiple variables
+# unpacking collection
+
+# your previous knowledge
+name = 'dan'
+colour = 'blue'
+country ='Nigeria'
+print(name)
+print(colour)
+print(country)
+
+# Assigning multiple variables
+# You can create multiple variables on one line and assign their values on the same line
+name, colour, country = 'dan', 'blue', 'Nigeria'
+print(name, colour, country)
+# OR
+print(name)
+print(colour)
+print(country)
---
+++ Variables/03_assigning_variables_lab.py
@@ -0,0 +1,26 @@
+# Task  1
+# Create a variable using pascal case. Call the variable my  account name and assign a value to it.
+# Print the content of your variable to the terminal
+
+# Task 2
+# You have picked three items from the grocery store, 
+# store the items in a variable using the assigning multiple variables you've learnt
+# Print out your three items stored in your vaiables.
+
+MyAccount = 'Checking Account'   # not correct
+print(MyAccount)
+
+#Three items from The Grocery Store
+
+dairy, produce, wine = 'cheese', 'lamb', 'Pinot Noir' # 10/10
+print(dairy, produce, wine)
+
+
+# correction; not writing for myself but for solutions; make sure I understand what is asked of me per directions
+MyAccountName = 'Checking Account'  # 10/10
+print('Account Name:',MyAccount)
+
+dairy, produce, wine = 'cheese', 'lamb', 'Pinot Noir' # 10/10
+print('Item1:', dairy)
+print('Item2:', produce)
+print('Item3:', wine)
